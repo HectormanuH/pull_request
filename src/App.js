85b564d0e45b64162e2ccf25f33b2b16d91bfe7c
Hector Manuel <hector.huechaqueo@gmail.com>
2024-10-03 12:54:31 -0300

    cambio de título 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Mis nueces 2
+          Mis crypto nueces 
         </a>
       </header>
     </div>
